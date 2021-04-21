e344e1e378189a75858dbb2a907f26913a4806f8
antonedu <55575226+antonedu@users.noreply.github.com>
2021-04-21 17:02:18 +0200

    Added disable scroll when navbar is open

--- index/script.js
+++ index/script.js
@@ -52,13 +52,13 @@ var id = setInterval(shuffleImages, 8000);
 function imageShuffleAdd () {
   clearInterval(id);
   shuffleImages(false);
-  id = setInterval(shuffle, 8000);
+  id = setInterval(shuffleImages, 8000);
 }
 
 function imageShuffleSub () {
   clearInterval(id);
   shuffleImages(true);
-  id = setInterval(shuffle, 8000);
+  id = setInterval(shuffleImages, 8000);
 }
 
 function toggleFooter() {
@@ -76,3 +76,44 @@ window.addEventListener('mousewheel', function(e){
       element.classList.add("hide");
     }
 });
+
+// disable scroll when nav is Open
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+function preventDefault(e) {
+  e.preventDefault();
+}
+
+function preventDefaultForScrollKeys(e) {
+  if (keys[e.keyCode]) {
+    preventDefault(e);
+    return false;
+  }
+}
+
+// modern Chrome requires { passive: false } when adding event
+var supportsPassive = false;
+try {
+  window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+    get: function () { supportsPassive = true; }
+  }));
+} catch(e) {}
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+// call this to Disable
+function disableScroll() {
+  window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+  window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+  window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+  window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+// call this to Enable
+function enableScroll() {
+  window.removeEventListener('DOMMouseScroll', preventDefault, false);
+  window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
+  window.removeEventListener('touchmove', preventDefault, wheelOpt);
+  window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+}
