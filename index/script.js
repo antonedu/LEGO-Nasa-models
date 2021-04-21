19c9d0dce9645bfa82087941cff99f096d134a73
antonedu <55575226+antonedu@users.noreply.github.com>
2021-04-21 09:35:25 +0200

    Added footer hide script

--- index/script.js
+++ index/script.js
@@ -42,3 +42,19 @@ function imageShuffleSub () {
   shuffleImages(true);
   id = setInterval(shuffle, 8000);
 }
+
+function toggleFooter() {
+  let element = document.getElementsByTagName("footer")[0];
+  element.classList.toggle("hide");
+}
+
+window.addEventListener('mousewheel', function(e){
+    let element = document.getElementsByTagName("footer")[0];
+    let wDelta = e.wheelDelta < 0 ? 'down' : 'up';
+    if (wDelta === 'down') {
+      element.classList.remove("hide");
+    }
+    else {
+      element.classList.add("hide");
+    }
+});
