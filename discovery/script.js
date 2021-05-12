26d2b0445dcf1f92dbaf8372f460355dc51ce2e8
antonedu <55575226+antonedu@users.noreply.github.com>
2021-05-12 09:35:09 +0200

    Made elements be added or removed on resize event

--- discovery/script.js
+++ discovery/script.js
@@ -1,69 +1,22 @@
-var arrayOfShuffleImages = [{
-    imagesrc: "../assets/10283.png",
-    set: "10283",
-    name: "NASA Space Shuttle Discovery"
-  },
-  {
-    imagesrc: "../assets/10266.png",
-    set: "10266",
-    name: "NASA Apollo 11 Lunar Lander"
-  },
-  {
-    imagesrc: "../assets/92176.png",
-    set: "92176",
-    name: "NASA Apollo Saturn V"
-  }
-];
-var currentImage = 0;
-
 function toggleMenu() {
   document.getElementById("navigation-links").classList.toggle("noshow");
 }
 
-function shuffleImages(sub) {
-  let header_front = document.getElementsByClassName('front')[0];
-  let header_back = document.getElementsByClassName('back')[0];
-  if (sub) {
-    if (currentImage === 2) {
-      currentImage = 0;
-    } else {
-      ++currentImage;
-    }
-  } else {
-    if (currentImage === 0) {
-      currentImage = 2;
-    } else {
-      --currentImage;
-    }
+function addOrRemoveElements() {
+  if (screen.width < 768) {
+    document.getElementById("navigation-links").classList.add("noshow");
   }
-  document.getElementsByClassName("back")[0].childNodes[0].src = arrayOfShuffleImages[currentImage].imagesrc;
-  document.querySelector(".text > h1").innerHTML = arrayOfShuffleImages[currentImage].set;
-  document.querySelector(".text > h2").innerHTML = arrayOfShuffleImages[currentImage].name;
-  header_front.classList.toggle("front");
-  header_front.classList.toggle("back");
-  header_back.classList.toggle("front");
-  header_back.classList.toggle("back");
+
+  if (screen.width > 1023) {
+    document.getElementById("navigation-links").classList.remove("noshow");
+  }
 }
 
-/*if (screen.width < 768) {
-  var id = setInterval(shuffleImages, 8000);
-}*/
+addOrRemoveElements();
 
-if (screen.width > 1023) {
-  document.getElementById("navigation-links").classList.remove("noshow");
-}
-
-function imageShuffleAdd() {
-  clearInterval(id);
-  shuffleImages(false);
-  id = setInterval(shuffleImages, 8000);
-}
-
-function imageShuffleSub() {
-  clearInterval(id);
-  shuffleImages(true);
-  id = setInterval(shuffleImages, 8000);
-}
+window.addEventListener('resize', () => {
+  addOrRemoveElements();
+})
 
 function toggleFooter() {
   let element = document.getElementsByTagName("footer")[0];
